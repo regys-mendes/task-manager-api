d73bfdaebfa96de887334f3d5a951eb50bb734ae
regys-mendes <regyseduardomendes123@gmail.com>
2026-06-26 19:17:20 -0300

    implement task findById and findByPriority endpoints

--- src/main/java/com/regysmendes/taskmanger/resource/TaskResource.java
+++ src/main/java/com/regysmendes/taskmanger/resource/TaskResource.java
@@ -1,9 +1,11 @@
 package com.regysmendes.taskmanger.resource;
 
 import com.regysmendes.taskmanger.entities.Task;
+import com.regysmendes.taskmanger.entities.TaskPriority;
 import com.regysmendes.taskmanger.services.TaskService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,6 +27,17 @@ public class TaskResource {
         return ResponseEntity.ok().body(obj);
     }
 
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Task> findById(@PathVariable Long id){
+        Task obj = service.findById(id);
+        return ResponseEntity.ok().body(obj);
+    }
+
+    @GetMapping(value = "/priority/{priority}")
+    public ResponseEntity<List <Task>> findByPriority(@PathVariable TaskPriority priority){
+        List<Task> obj = service.findByPriority(priority);
+        return ResponseEntity.ok().body(obj);
+    }
 
 
 }
